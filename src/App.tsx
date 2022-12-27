5a3f3d4a33800de1d6c344c979f4c5502a6898bb
Andrey <andrey@itsua.org>
2022-12-27 12:00:02 +0200

    init app

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Awesome Rusty dashboard, TS and tailwind added
+      </h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
