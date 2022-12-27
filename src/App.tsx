0bb7ee9b9bdaacd727299f09bebec079bbb1e68b
Andrey <andrey@itsua.org>
2022-12-27 13:08:15 +0200

    eslint added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useState } from 'react'
 import './App.css'
 
-function App() {
+function App (): JSX.Element {
   const [count, setCount] = useState(0)
 
   return (
